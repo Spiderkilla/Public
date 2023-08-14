0db3e3ddb5009b16419781e80397491626eec98c
Spiderkilla <38021310+Spiderkilla@users.noreply.github.com>
2023-08-14 19:26:00 +0200

    Update Win11AzCLIPsScript.ps1

--- AIB/1_Creating_a_Custom_Win_Shared_Image_Gallery_Image/Win11AzCLIPsScript.ps1
+++ AIB/1_Creating_a_Custom_Win_Shared_Image_Gallery_Image/Win11AzCLIPsScript.ps1
@@ -1,1 +1,3 @@
-
+Invoke-WebRequest -Uri https://azcliprod.blob.core.windows.net/msi/azure-cli-2.51.0-x64.msi -OutFile .\AzureCLI.msi
+Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'
+rm .\AzureCLI.msi
