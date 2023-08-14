51ee5859fcc8324c1df7d8736652c20288f4ae24
Spiderkilla <38021310+Spiderkilla@users.noreply.github.com>
2023-08-14 20:45:43 +0200

    Update Win11RSATPsScript.ps1

--- AIB/1_Creating_a_Custom_Win_Shared_Image_Gallery_Image/Win11RSATPsScript.ps1
+++ AIB/1_Creating_a_Custom_Win_Shared_Image_Gallery_Image/Win11RSATPsScript.ps1
@@ -1,1 +1,6 @@
-Get-WindowsCapability -Name RSAT* -Online | Add-WindowsCapability -Online -Verbose
+$List = "Rsat.ActiveDirectory*","Rsat.BitLocker*","Rsat.DHCP*","Rsat.Dns*","Rsat.FileServices*","Rsat.GroupPolicy*","Rsat.FailoverCluster*"
+
+foreach ($Name in $List)
+{
+  Get-WindowsCapability -Name $Name -Online| Add-WindowsCapability -Online -Verbose
+}
