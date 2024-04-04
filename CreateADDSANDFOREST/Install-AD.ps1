6ed32ecaea55b0b2ef3262224a9d68f58cade29c
Spiderkilla <38021310+Spiderkilla@users.noreply.github.com>
2024-04-04 16:11:42 +0200

    Create Install-AD.ps1

---
+++ CreateADDSANDFOREST/Install-AD.ps1
@@ -0,0 +1,33 @@
+function Install-AD {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$DomainName,
+
+        [Parameter(Mandatory=$true)]
+        [string]$DomainNetbiosName,
+
+        [Parameter(Mandatory=$true)]
+        [string]$secret
+    )
+    # Convert secret to secure string
+    $SafeModeAdministratorPassword = ConvertTo-SecureString $secret -AsPlainText -Force
+
+    # Install AD DS role
+    Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools -Verbose
+
+    # Prepare the forest and domain
+    Install-ADDSForest `
+        -CreateDnsDelegation:$false `
+        -DatabasePath "C:\Windows\NTDS" `
+        -DomainMode "7" `
+        -DomainName $DomainName `
+        -DomainNetbiosName $DomainNetbiosName `
+        -ForestMode "7" `
+        -InstallDns:$true `
+        -LogPath "C:\Windows\NTDS" `
+        -NoRebootOnCompletion:$false `
+        -SysvolPath "C:\Windows\SYSVOL" `
+        -SafeModeAdministratorPassword $SafeModeAdministratorPassword `
+        -Force:$true `
+        -Verbose
+}
