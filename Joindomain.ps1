9f9aca0f7459785154cab40408de6f2ae523722a
Spiderkilla <38021310+Spiderkilla@users.noreply.github.com>
2024-04-08 09:45:36 +0200

    Delete Joindomain.ps1

--- Joindomain.ps1
+++
@@ -1,9 +0,0 @@
-#Join VM TO A domain using VM extension
-
-$getpass = Get-AzKeyVaultSecret -VaultName pvkv9207 -Name vm -AsPlainText
-$password = convertto-securestring $getpass -AsPlainText -Force 
-$user = "Domain\azureuser"
-$credential = New-Object System.Management.Automation.PSCredential ($user,$password) 
-$domainname = "Domain.LOCAL"
-
-set-azVMADDomainExtension -ResourceGroupName "AVD" -VMName "domains" -Name "DemoVmAdDomainExtension" -DomainName $domainname -Credential $credential -JoinOption 0x00000003 -Restart
