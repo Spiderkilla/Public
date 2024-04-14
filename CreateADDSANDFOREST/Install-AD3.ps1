5998cbb3e6d276aca2865ac736565cbe4ae11ba8
Spiderkilla <38021310+Spiderkilla@users.noreply.github.com>
2024-04-14 17:34:47 +0200

    Create Install-AD3.ps1

---
+++ CreateADDSANDFOREST/Install-AD3.ps1
@@ -0,0 +1,29 @@
+
+# Specify the OU name
+$OUName = "AVDLABS"
+# Create an organizational unit (OU) for users
+New-ADOrganizationalUnit -Name $OUName 
+
+# Get the OU object
+$OU = Get-ADOrganizationalUnit -Filter "Name -eq '$OUName'"
+
+# Retrieve the canonical name (DN)
+$OUDN = $OU.DistinguishedName
+
+# Create users
+$users= "user1avdlab", "user2avdlab", "user3avdlab"
+
+$Password = ConvertTo-SecureString "P@ssw0rd1234" -AsPlainText -Force
+
+#New-ADUser -Name "John Smith" -SamAccountName "jsmith" -UserPrincipalName $env:USERDNSDOMAIN -GivenName "John" -Surname "Smith" -AccountPassword $Password -Enabled $true -Path $OUDN
+
+foreach ($user in $users) {
+$UPN = $user + "@" + $env:USERDNSDOMAIN
+
+    New-ADUser -Name $user `
+               -SamAccountName $user `
+               -UserPrincipalName $UPN `
+               -AccountPassword $Password `
+               -Enabled $true `
+               -Path $OUDN
+}
